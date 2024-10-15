460805938935b61584eb593816703e808343862d
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2024-10-15 09:25:27 +0000

    add search, inOrder travarsal

--- bst/bst.c
+++ bst/bst.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+int scanInt(char* prompt){
+    int n;
+    printf("%s", prompt);
+    scanf("%d", &n);
+    return n;
+}
+
 typedef struct Node{
     int data;
     struct Node* left;
@@ -11,19 +19,50 @@ Node* createNode(int data){
     newNode->left = newNode->right = NULL;
     return newNode;
 }
-Node* insertNode(Node* root, int data){
+Node* insert(Node* root, int data){
     if(root==NULL){
         return createNode(data);
     }
     if(data < root->data){
-        root->left = insertNode(root->left, data);
+        root->left = insert(root->left, data);
     }else if(data>= root->data){
-        root->right = insertNode(root->right, data);
+        root->right = insert(root->right, data);
     }
     return root;
 }
+void inOrder(Node* root){
+     if(root==NULL) return;
+     inOrder(root->left);
+     printf("%d ", root->data);
+     inOrder(root->right);
+}
+Node* search(Node* root, int key){
+    if(root==NULL) return NULL;
+    if(root->data==key) return root;
+    if(key < root->data){
+        return search(root->left, key);
+    }else if(key >= root->data){
+        return search(root->right, key);
+    }
+}
 int main(){
     Node* root = NULL;
-    root = insertNode(root, 10);
-    printf("%d", root->data);
+    root = insert(root, 8);
+    insert(root, 3);
+    insert(root, 10);
+    insert(root, 1);
+    insert(root, 6);
+    insert(root, 14);
+    insert(root, 4);
+    insert(root, 7);
+    insert(root, 13);
+    inOrder(root);
+    printf("\n");
+    Node* node = search(root, scanInt(""));
+    if(node==NULL){
+        printf("Element not found :" );
+    }else{
+        printf("Node found %d", node->data);
+    }
+
 }
